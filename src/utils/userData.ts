44ccb8f6cf00178f52421b989483fa880f135043
MatheusFerreira33 <matheus.fsilva33@gmail.com>
2023-07-18 14:23:28 -0300

    feat:alterações

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Matheus Ferreira",
+  githubUser: "MatheusFerreira33",
+  linkedinUser: "matheusferreira33",
+  whatsappNumber: "+5511932211306",
+  emailUser: "matheus.fsilva33@gmail.com",
+  instagramUser: "",
   facebookUser: "",
 };
